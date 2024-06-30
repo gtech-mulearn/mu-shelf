77c838efe2138dcf2beb545f0e9ec22509567803
Kethankrk <kethankrk@gmail.com>
2024-06-30 22:03:29 +0530

    added user model and user manager

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 import uuid
+from .managers import user_manager
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from decouple import config as decouple_config
 
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    discord_id = models.CharField(unique=True, max_length=36, blank=True, null=True)
+    muid = models.CharField(unique=True, max_length=100)
+    full_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True, max_length=200)
+    password = models.CharField(max_length=200, blank=True, null=True)
+    mobile = models.CharField(unique=True, max_length=15, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True, choices=[("Male", "Male"), ("Female", "Female")])
+    dob = models.DateField(blank=True, null=True)
+    admin = models.BooleanField(default=False)
+    exist_in_guild = models.BooleanField(default=False)
+    district = models.ForeignKey("District", on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    suspended_at = models.DateTimeField(blank=True, null=True)
+    suspended_by = models.ForeignKey("self", on_delete=models.SET(settings.SYSTEM_ADMIN_ID), blank=True, null=True,
+                                     related_name="user_suspended_by_user", db_column="suspended_by", default=None)
+    objects = user_manager.ActiveUserManager()
+    every = models.Manager()
+
+    class Meta:
+        managed = False
+        db_table = 'user'
+
+    @property
+    def profile_pic(self):
+        fs = FileSystemStorage()
+        path = f'user/profile/{self.id}.png'
+        if fs.exists(path):
+            return f"{decouple_config('BE_DOMAIN_NAME')}{fs.url(path)}"
+
+    def save(self, *args, **kwargs):
+        if self.muid is None:
+            full_name = self.full_name.replace(" ", "-").lower()
+            self.muid = f"{full_name}@mulearn"
+
+            counter = 0
+            while User.objects.filter(muid=self.muid).exists():
+                counter += 1
+                self.muid = f"{full_name}-{counter}@mulearn"
+
+        return super().save(*args, **kwargs)
+
+
+class Company(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class ProblemStatement(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
