ecf79805d0a47a51e6dea64ef350c5e7709d85eb
Kethankrk <kethankrk@gmail.com>
2024-07-04 11:47:35 +0530

    added user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,14 +11,8 @@ class User(models.Model):
     muid = models.CharField(unique=True, max_length=100)
     full_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True, max_length=200)
-    password = models.CharField(max_length=200, blank=True, null=True)
     mobile = models.CharField(unique=True, max_length=15, blank=True, null=True)
-    gender = models.CharField(max_length=10, blank=True, null=True, choices=[("Male", "Male"), ("Female", "Female")])
-    dob = models.DateField(blank=True, null=True)
-    admin = models.BooleanField(default=False)
     exist_in_guild = models.BooleanField(default=False)
-    district = models.ForeignKey("District", on_delete=models.CASCADE, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     suspended_at = models.DateTimeField(blank=True, null=True)
     suspended_by = models.ForeignKey("self", on_delete=models.SET(settings.SYSTEM_ADMIN_ID), blank=True, null=True,
                                      related_name="user_suspended_by_user", db_column="suspended_by", default=None)
@@ -29,46 +23,21 @@ class User(models.Model):
         managed = False
         db_table = 'user'
 
-    @property
-    def profile_pic(self):
-        fs = FileSystemStorage()
-        path = f'user/profile/{self.id}.png'
-        if fs.exists(path):
-            return f"{decouple_config('BE_DOMAIN_NAME')}{fs.url(path)}"
-
-    def save(self, *args, **kwargs):
-        if self.muid is None:
-            full_name = self.full_name.replace(" ", "-").lower()
-            self.muid = f"{full_name}@mulearn"
-
-            counter = 0
-            while User.objects.filter(muid=self.muid).exists():
-                counter += 1
-                self.muid = f"{full_name}-{counter}@mulearn"
-
-        return super().save(*args, **kwargs)
-
-
-class Company(models.Model):
-    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
 class ProblemStatement(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    reward = models.TextField()
+    reward = models.CharField(max_length=100)
     approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name="problem_statements", on_delete=models.CASCADE)
     start = models.DateTimeField(null=True)
     end = models.DateTimeField(null=True)
 
     class Meta:
         managed = False
-        db_table = 'problem_statement'
+        db_table = 'problem_statements'
 
 class Solution(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
@@ -78,6 +47,7 @@ class Solution(models.Model):
     problem_statement = models.ForeignKey(ProblemStatement, related_name="solutions", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name="solutions_submitted", on_delete=models.CASCADE)
 
     class Meta:
         managed = False
@@ -85,13 +55,13 @@ class Solution(models.Model):
 
 
 
-class Contributors(models.Model):
+class Contributor(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    contact = models.TextField(null=True)
+    contact = models.CharField(max_length=200)
     solution = models.ForeignKey(Solution, related_name="contributors", on_delete=models.CASCADE)
 
     class Meta:
         managed = False
-        db_table = 'contributors'
+        db_table = 'contributor'
