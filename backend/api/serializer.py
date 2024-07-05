f5bde86e02192f3f3201a7507c9efb3bd909cd4a
Kethankrk <kethankrk@gmail.com>
2024-07-05 10:28:51 +0530

    updated problem statement serializer

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -4,5 +4,5 @@ from .models import ProblemStatement
 class Problem_statement_serializer(serializers.ModelSerializer):
     class Meta:
         model = ProblemStatement
-        fields = ['id', 'title', 'description', 'reward', 'start', 'end', 'created_at', 'updated_at']
-        read_only_fields = ['id']
+        fields = ['id', 'title', 'description', 'reward', 'start', 'end', 'created_at', 'updated_at', 'created_by']
+        read_only_fields = ['id', 'created_by']
