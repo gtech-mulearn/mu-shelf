e6b768f00107ddf12c595ef36c59413a7ad958fa
Kethankrk <kethankrk@gmail.com>
2024-07-05 10:30:50 +0530

    added route for problem statement detail view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('statement/', views.ProblemStatementAPI.as_view(), name="problem-statement")
+    path('statement/', views.ProblemStatementAPI.as_view(), name="problem-statement"),
+    path('statement/<str:pk>/', views.ProblemStatementDetailAPI.as_view(), name="problem-statement-detail")
 ]
