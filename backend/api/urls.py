a76454f85e2c9e347e98315d7762855300f78a09
Kethankrk <kethankrk@gmail.com>
2024-08-02 16:53:39 +0530

    feat: added company urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("problem-statement/", include("api.problem_statement.urls")),
     path("solution/", include("api.solution.urls")),
+    path("company/", include("api.company.urls")),
 ]
