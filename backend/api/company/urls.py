cd092cb0d766b2ac6c939aa85fbdb567f7ef04ef
Kethankrk <kethankrk@gmail.com>
2024-08-18 21:43:30 +0530

    added pubilc profile and dashboard url

--- backend/api/company/urls.py
+++ backend/api/company/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .company_view import CompanyProfileCreateAPI
+from .company_view import CompanyProfileCreateAPI, CompanyProfilePublicAPI, CompanyDashboardAPI
 
 urlpatterns = [
-    path("profile/", CompanyProfileCreateAPI.as_view(), name="company_profile_create")
+    path("profile/", CompanyProfileCreateAPI.as_view(), name="company_profile_create"),
+    path("profile/<str:pk>/", CompanyProfilePublicAPI.as_view(), name="company_profile_get_update"),
+    path("dashboard/", CompanyDashboardAPI.as_view(), name="company_dashboard"),
 ]
