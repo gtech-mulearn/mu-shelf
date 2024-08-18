408d951ce01decce42fe49aa6e37d62939b0397c
Kethankrk <kethankrk@gmail.com>
2024-08-18 15:43:08 +0530

    modified company profile creation view

--- backend/api/company/company_view.py
+++ backend/api/company/company_view.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from .serializer import CompanyProfileSerializer
 from utils.response import CustomResponse
-
+from utils.permission import role_required, CustomizePermission, JWTUtils
+from utils.types import RoleType
 
 class CompanyProfileCreateAPI(APIView):
+    permission_classes = [CustomizePermission]
 
+    @role_required([RoleType.COMPANY_MEMBER.value])
     def post(self, request):
         data = request.data
-        data['created_by'] = request.user.id
-        serializer = CompanyProfileSerializer(data=data)
+        user_id = JWTUtils.fetch_user_id(request)
+        serializer = CompanyProfileSerializer(data=data, context={"user_id": user_id})
         if not serializer.is_valid():
             return CustomResponse(message=serializer.errors).get_failure_response()
         serializer.save()
+        return CustomResponse(general_message="Successfully created company profile")
